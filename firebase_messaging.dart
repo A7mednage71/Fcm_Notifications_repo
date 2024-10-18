fc3c154047f907dc6bedc1ed31513c93b209aa35
A7med_Nage7 <125461743+A7mednage71@users.noreply.github.com>
2024-10-18 15:16:49 +0300

    Update firebase_messaging.dart

--- firebase_messaging.dart
+++ firebase_messaging.dart
@@ -109,19 +109,18 @@ class FirebaseMessagingHelper {
   Future<String?> getAccessToken() async {
     final serviceAccountJson = {
       "type": "service_account",
-      "project_id": "docdoc-distribution-cfe82",
-      "private_key_id": "7b4d5f7bdb34f85339a1695a11687719b47b701b",
-      "private_key":
-          "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCr377MnYgsrfAG\nzF1nyvSjreOoTczhK1jvwcfBu1C4Nwhvzg3thG9IcbvihwgIsJKXBJua9Loo99iS\naeAD5QV25/Gv6Lcos6R53p5sVIfJjF8SUj1P/ygGBMZH2gmIVaAZs6WDnnNJq9kz\nNRNxzllPbOh0iy0dthkRfhS3trcHwopOqjCzn2DX1MoBJQL1iuPULAoUbzGfdBeA\nYj2YsaGm5RYYZXOHm7h2JUvDcxMDd3FOC9AK/q/leoWqPfSYljQSpy5yMfEFMK0D\nzGDvyU8Dtws2kIdT80dZ79A71zg1RStTMIppCxEe9RAujJZKOekJIMaw1EY7sqFE\nYrrbSJTbAgMBAAECggEARWXchoX9G/1Hc5dFB8m9KfHmgGiZlzmHeZeG7sSRfTBL\nacmLeiIFRP0XXgojxk51giDMK68xE6Wvfr7dQvVQVYil399ZRUfz23l2AkvHYCwb\nnywxsYFXScbXwN9bBf9826Pb6t8psc9/rdt6dHNbQGS7H4OqvdpvGM8N0ngmQy0y\neai2ruV2+gUlmgc+6d4TtIsF+vQgD6ZZgTWk2J0DHo9KHS6pGCjsMrBHgWrmZHDm\netnPN+8Y5vCQJBlilnZ0Jmc2ChwiqI7kd6IUcPV5qJuECoMr4QaUwDYyAMAMMPj6\nQwOJjQuAAyjp9k0D3p+Z0c7QxEsFzoYiGj4rut26UQKBgQDWA5mA0jwurFuEiCR9\nPNWAMltq3AoS8pD2VUgJgXBaeF73xf/81lhwDOZuh7bULM6EQHoMkUcXyP8ma584\nuTdQ3q7LK2UD/9i+aPkP764gzuyKklPCx+OvwcRunMuRzg4PeyEu/UzE7BlORUiQ\nJNUWkrYGtlsR21zEKNAV4oqA9QKBgQDNl8BKa7BqxOf3wfn0P6ySBhdV7auJ3kK4\nJiNV3u7RPMegPhKrMkJU0ThDtLezTbLcuavnGTkf0j81BRAXEEEVu6Xrku9lv7F3\njGLcuai6Z5a9Lse+hx9h+NhCEvY90I55gpl/++EnRnz6R+2weXLnTn6xUiS/V3R8\nfzv7lbjcjwKBgBivK5pfJU/g02Fy7np/dMSnikHGBWdwEOZIqdlm05WrwpBjhwYb\nlvG9mypuftj0HhHE+g7PBtsodL1ytletjULHnHOUmr8eWFqF8wwygewI0eGdxQl3\nUryn5cc4UIaNtLN2aTppPtyLutN7TEZL6UQEQfh+OfzSR13cszuC+KStAoGALAzp\nvIhaYmYSNbmwLq898IOxmE22RXID5aT2ST3c+aQGOcVTBq9cGwRBA/DCs35gZn65\n4Gg9Hx5TQK73BZoL9/Ye1NzEwo5SHgVMYXK+PkJXv+04CxC0nq9M1sttS01WWZ6r\n+Qok8d9eg9nJidhb0Ee3SZMKIJ1CbjJszbkExO0CgYAG17xL+Szyf+UPPjCEDr7F\nWaSL9mGqUU5XCCY3GMQP//cH7q/1/tqC5PbN/EsyBMGLYOSSAUx0qCNFzTn+hvXn\nAnV6zDLnE++j7fTHWHMYkSvqN1FxpNAzif6j4Zf2mv72fAjqMIPz8YYtwFY6zEtV\npo3ewbY9NZnF8zioaelDdg==\n-----END PRIVATE KEY-----\n",
+      "project_id": "****************************",
+      "private_key_id": "*****************************",
+      "private_key":"******************************",
       "client_email":
-          "firebase-adminsdk-w8f3a@docdoc-distribution-cfe82.iam.gserviceaccount.com",
-      "client_id": "112716882023521959532",
+          "************************************",
+      "client_id": "**************************************",
       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
       "token_uri": "https://oauth2.googleapis.com/token",
       "auth_provider_x509_cert_url":
-          "https://www.googleapis.com/oauth2/v1/certs",
+          "*******************************",
       "client_x509_cert_url":
-          "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-w8f3a%40docdoc-distribution-cfe82.iam.gserviceaccount.com",
+          "***************************************",
       "universe_domain": "googleapis.com"
     };
 
@@ -158,7 +157,7 @@ class FirebaseMessagingHelper {
       var token = await fcm.getToken();
 
       const String urlEndPoint =
-          "https://fcm.googleapis.com/v1/projects/docdoc-distribution-cfe82/messages:send";
+          "https://fcm.googleapis.com/v1/projects/*******PUT APP ID HERE*****/messages:send";
 
       Dio dio = Dio();
       dio.options.headers['Content-Type'] = 'application/json';
@@ -193,7 +192,7 @@ class FirebaseMessagingHelper {
 
       var serverKeyAuthorization = await getAccessToken();
       const String urlEndPoint =
-          "https://fcm.googleapis.com/v1/projects/docdoc-distribution-cfe82/messages:send";
+          "https://fcm.googleapis.com/v1/projects/***********PUT APP ID*********/messages:send";
 
       Dio dio = Dio();
       dio.options.headers['Content-Type'] = 'application/json';
